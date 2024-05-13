fc8bae83ec9f418f28fdba3047596e3a719883b5
Monika Singh <136189924+monikakksingh@users.noreply.github.com>
2024-05-13 20:13:08 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,98 @@
+// objects
+
+// object literal
+
+// let obj ={};
+
+// let user ={
+//     name: 'Alka',
+//     age: 20,
+// };
+// console.log(user);
+// console.log(user.name);
+// console.log(user.age);
+
+// user.city = 'delhi';
+// console.log(user);
+
+// delete user.age;
+// console.log(user);
+
+// let keys = object.keys(user);
+// console.log(keys);
+
+// let user1={
+//     name:'alka',
+//     age:20,
+//     'email id':"alka@navgurukul.org", 
+// };
+// console.log(user1["email id"]);
+// console.log(user1);
+// console.log(user1.name);
+// console.log(user1.age);
+// console.log(user1.email id);
+// console.log(user1["name"]);
+
+// let fName="Vaishnavi";
+// let user2 = {
+//     [fName]: "Anjali",
+// };
+
+// console.log(user2);
+// console.log(user2.fName);
+// console.log(user2.Vaishnavi);
+
+// let fruit = "apple";
+// let bag = { };
+// bag[fruit] = 5;
+
+// console.log(bag);
+
+// let bag ={
+//     fruit: 3,
+// };
+// bag.fruit=5;   //value modified
+// console.log(bag);
+
+// let fruit = "apple";
+// let bag = {};
+
+// bag[fruit] = 5;
+
+// console.log(bag);
+
+// let fruit = "apple";
+// let bag = {
+//     [fruit + "computer"]: 5,
+// };
+// console.log(bag);
+
+// function makeUser(name, age){
+//     return {
+//         name: name,
+//         age: age,
+//     };
+// }
+// let user = makeUser("Alka",20);
+// console.log(user);
+
+// function userDetails(name,age){
+//     return{
+//         name,
+//         age: age || 18,
+//     }
+// }
+// let user1 = userDetails("Alka", 20);
+// console.log(user1);
+
+// let user2 = userDetails("Alka");
+// console.log(user2);
+
+// let obj = {
+//     1 : "one",
+// };
+
+// console.log(obj);
+// console.log(obj[1]);
+// console.log(obj["1"]);
+
